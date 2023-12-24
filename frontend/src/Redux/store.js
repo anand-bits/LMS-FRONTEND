7796f123f4d689ec19576f63d3455517727753be
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-12-24 15:14:51 +0530

    Started Basics Redux Toolkit

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authSliceReducer from "./Slices/AuthSlice";
 const store= configureStore(
     {
-        reducer:{},
+        reducer:{auth:authSliceReducer},
         devTools:true
 
     }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,7 @@ import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import store from './Redux/Slices/store.js'
+import store from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <Provider store={store}>
