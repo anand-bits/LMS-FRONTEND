4addebfd87b6b1fabab48c8517a2856c8b8d0371
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-12-24 15:06:18 +0530

    with toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,15 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import store from './Redux/Slices/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+ <Provider store={store}>
  <BrowserRouter>
             <App/>
             <Toaster/>
  </BrowserRouter>
+ </Provider>
 )
