a7dc456926fe79b6a72825a66d2944003da4e5d5
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-12-31 00:13:53 +0530

    Complted With Footer, Layout nd Home Page

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,24 +1,14 @@
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { FiMenu } from 'react-icons/fi';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Footer from "../components/footer";
 import { useDispatch, useSelector } from 'react-redux';
+import { Button,Box , Typography} from '@mui/material';
 
 function HomeLayout({ children }) {
-
-const dispatch=useDispatch();
-const navigate=useNavigate();
-
-
-const isLoggedIn=useSelector((state)=>state?.auth?.isLoggedIn);
-
-
-const role=useSelector((state)=>state?.auth?.role);
-
-
-
-
-
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+    const role = useSelector((state) => state?.auth?.role);
 
     function openDrawer() {
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -34,11 +24,11 @@ const role=useSelector((state)=>state?.auth?.role);
     }
 
     return (
-        <div className="min-h-[90vh] m-0 p-0">
-            <div className="drawer">
+        <div className="min-h-[90vh]">
+            <div className="drawer m-0">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content">
-                    <label htmlFor="my-drawer">
+                <div className="drawer-content pl-4"> {/* Added padding-left (pl-4) for space */}
+                    <label htmlFor="my-drawer" className="m-0 p-0">
                         <FiMenu
                             onClick={openDrawer}
                             size={"40px"}
@@ -61,7 +51,7 @@ const role=useSelector((state)=>state?.auth?.role);
                             <Link to="/"> Home </Link>
                         </li>
 
-                        {isLoggedIn && role==='ADMIN' &&(
+                        {isLoggedIn && role === 'ADMIN' && (
                             <li>
                                 <Link to="/admin/dashboard">Admin Dashboard</Link>
                             </li>
@@ -76,13 +66,49 @@ const role=useSelector((state)=>state?.auth?.role);
                         <li>
                             <Link to="/courses"> All courses </Link>
                         </li>
+
+                        {!isLoggedIn && (
+                            <div className='w-full flex items-center justify-center space-x-2'>
+                                <Button variant='contained' color='primary'>
+                                    <Link to="/login"> Login</Link>
+                                </Button>
+
+                                <Button variant='contained' color='secondary'>
+                                    <Link to="/signup"> SignUp</Link>
+                                </Button>
+                            </div>
+                        )}
+
+                            {isLoggedIn && (
+                            <div className='w-full flex items-center justify-center space-x-2'>
+                                <Button variant='contained' color='primary'>
+                                    <Link to="/user/profile"> Login</Link>
+                                </Button>
+
+                                <Button variant='contained' color='secondary'>
+                                    <Link to="/logout"> Logout</Link>
+                                </Button>
+                            </div>
+                        )}
+                        
+
+
+
                     </ul>
                 </div>
             </div>
 
             {children}
 
+           
             <Footer />
+            <Box sx={{ textAlign: 'center', padding: 2, backgroundColor: 'yellow', marginTop: '10px', borderRadius: '8px' }}>
+                    {/* Typography for the footer with a heart symbol */}
+                    <Typography variant="body2" color="textSecondary">
+                        Made with ❤️ by Anand 
+                    </Typography>
+                </Box>
+
         </div>
     );
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,38 +1,58 @@
-import { Link } from "react-router-dom";
-import homePageMainImage from '../assets/Images/Anand.jpg';
-import HomeLayout from "../Layouts/HomeLayout";
+// Import necessary dependencies from Material-UI and other libraries
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Typography, Container, Grid, Card, Box } from '@mui/material';
+import homePageMainImage from '../assets/Images/Anand.jpg'; // Import the image
+import HomeLayout from '../Layouts/HomeLayout'; // Import the HomeLayout component
 
-function Home() {
+// Functional component for the home page
+const HomePage = () => {
     return (
+        // Use the HomeLayout component to wrap the content
         <HomeLayout>
-            <div className="bg-pink-400 pt-8 text-black flex items-center justify-center gap-10 mx-0 h-[90vh]">
-                <div className="w-1/2 space-y-6">
-                    <h1 className="text-5xl font-semibold">Find out best <span className="text-yellow-500 font-bold">Online courses</span></h1>
-                    <p className="text-xl text-gray-200">
-                        We have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost.
-                    </p>
-
-                    <div className="space-x-6">
-                        <Link to="/courses" className="text-black">
-                            <button className="bg-light-blue-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer transition-all ease-in-out duration-300 hover:bg-light-blue-600 hover:text-white">
-                                Explore courses
-                            </button>
-                        </Link>
-
-                        <Link to="/contacts" className="text-black">
-                            <button className="border border-light-blue-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer transition-all ease-in-out duration-300 hover:bg-light-blue-600 hover:text-white">
-                                Contact Us
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-
-                <div className="w-1/2 flex items-center justify-center">
-                    <img src={homePageMainImage} alt="home page" />
-                </div>
-            </div>
+            {/* Container for the main content */}
+            <Container style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f9f9f9' }}>
+                {/* Grid container with spacing and alignment */}
+                <Grid container spacing={4} alignItems="center" justifyContent="space-between" style={{ flex: 1 }}>
+                    {/* Left side content */}
+                    <Grid item md={6} style={{ backgroundColor: '#ffffff', padding: '20px', borderRadius: '8px' }}>
+                        {/* Typography for the main heading */}
+                        <Typography variant="h3" component="h1" color="textPrimary" gutterBottom>
+                            Discover the Best <span style={{ color: '#FFD700' }}>Online Courses</span>
+                        </Typography>
+                        {/* Typography for the paragraph */}
+                        <Typography variant="body1" color="textSecondary" paragraph>
+                            Explore a vast library of courses taught by highly skilled and qualified faculties at an affordable cost.
+                        </Typography>
+                        {/* Buttons for navigation */}
+                        <div style={{ display: 'flex', gap: '16px' }}>
+                            <Link to="/courses">
+                                <Button variant="contained" color="primary">
+                                    Explore Courses
+                                </Button>
+                            </Link>
+                            <Link to="/contact">
+                                <Button variant="outlined" color="primary">
+                                    Contact Us
+                                </Button>
+                            </Link>
+                        </div>
+                    </Grid>
+                    {/* Right side content */}
+                    <Grid item md={6} style={{ display: 'flex', justifyContent: 'flex-end', padding: '20px', borderRadius: '8px', overflow: 'hidden' }}>
+                        {/* Card component with image */}
+                        <Card sx={{ maxWidth: 400, boxShadow: 5, backgroundColor: '#f9f9f9' }}>
+                            {/* Image with styling */}
+                            <img src={homePageMainImage} alt="home page" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }} />
+                        </Card>
+                    </Grid>
+                </Grid>
+                
+              
+            </Container>
         </HomeLayout>
-    )
-}
+    );
+};
 
-export default Home;
+// Export the HomePage component
+export default HomePage;
