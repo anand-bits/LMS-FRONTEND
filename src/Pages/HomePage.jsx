a2bba2143d97a6970ebeaad06720c3186f915498
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-12-31 00:31:57 +0530

    Working Fine Now API addittion and multiple page to be added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,11 +11,11 @@ const HomePage = () => {
         // Use the HomeLayout component to wrap the content
         <HomeLayout>
             {/* Container for the main content */}
-            <Container style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f9f9f9' }}>
+            <Container style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f6f1c2 '}}>
                 {/* Grid container with spacing and alignment */}
-                <Grid container spacing={4} alignItems="center" justifyContent="space-between" style={{ flex: 1 }}>
+                <Grid container spacing={3} alignItems="center" justifyContent="space-between" style={{ flex: 1 }}>
                     {/* Left side content */}
-                    <Grid item md={6} style={{ backgroundColor: '#ffffff', padding: '20px', borderRadius: '8px' }}>
+                    <Grid item md={6} style={{ backgroundColor: ' #f6f1c2', padding: '10px', borderRadius: '8px' }}>
                         {/* Typography for the main heading */}
                         <Typography variant="h3" component="h1" color="textPrimary" gutterBottom>
                             Discover the Best <span style={{ color: '#FFD700' }}>Online Courses</span>
@@ -32,16 +32,16 @@ const HomePage = () => {
                                 </Button>
                             </Link>
                             <Link to="/contact">
-                                <Button variant="outlined" color="primary">
+                                <Button variant="contained" color="secondary">
                                     Contact Us
                                 </Button>
                             </Link>
                         </div>
                     </Grid>
                     {/* Right side content */}
-                    <Grid item md={6} style={{ display: 'flex', justifyContent: 'flex-end', padding: '20px', borderRadius: '8px', overflow: 'hidden' }}>
+                    <Grid item md={6} style={{ display: 'flex', justifyContent: 'flex-end', padding: '10px'}}>
                         {/* Card component with image */}
-                        <Card sx={{ maxWidth: 400, boxShadow: 5, backgroundColor: '#f9f9f9' }}>
+                        <Card sx={{ maxWidth: 400, boxShadow: 6, backgroundColor: '#f9f9f9' }}>
                             {/* Image with styling */}
                             <img src={homePageMainImage} alt="home page" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }} />
                         </Card>
